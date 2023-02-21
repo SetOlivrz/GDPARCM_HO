cceedec8869603d7c72aa761e49dfca845095af7
Set Olivarez <setiel_b_olivarez@dlsu.edu.ph>
2023-02-21 15:19:11 +0800

    update

---
+++ GDPARCM_VS/HO-Batch_Asset_Loader/StreamAssetLoader.cpp
@@ -0,0 +1,32 @@
+#include "StreamAssetLoader.h"
+#include <iostream>
+#include "TextureManager.h"
+#include "StringUtils.h"
+#include "IExecutionEvent.h"
+
+StreamAssetLoader::StreamAssetLoader(String path, IExecutionEvent* executionEvent)
+{
+	this->path = path;
+	this->executeEvent = executionEvent;
+}
+
+StreamAssetLoader::~StreamAssetLoader()
+{
+	std::cout << "Destroying stream asset loader. " << std::endl;
+}
+
+void StreamAssetLoader::run()
+{
+	//delay
+	IETThread::sleep(500);
+
+	// from texturemanager
+	std::vector<String> tokens = StringUtils::split(path, '/');
+	String assetName = StringUtils::split(tokens[tokens.size() - 1], '.')[0];
+	TextureManager::getInstance()->instantiateAsTexture(path, assetName, true);
+
+	std::cout << "[TextureManager] Loaded streaming texture: " << assetName << std::endl;
+
+	this->executeEvent->onFinishedExecution();
+	delete this;
+}
